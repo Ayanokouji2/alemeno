13731b25d0c8dbfba885463b04789d8f9fed0c9f
Shivam_Sharma <shivam.sharma2023@vitstudent.ac.in>
2024-08-31 21:41:10 +0530

    chore: Update npm dependencies for react-icons to version 5.3.0

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,24 @@
-import {Link, NavLink} from 'react-router-dom'
+import {useEffect, useState} from 'react'
+import {Link, NavLink, useLocation} from 'react-router-dom'
+import {FiSearch} from 'react-icons/fi'
 
 export default function Navbar() {
+	const location = useLocation()
+
+	const [searchText, setSearchText] = useState('')
+
+	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchText(e.target.value)
+	}
+
+	useEffect(() => {
+		const delay = 3000
+		const searchTimeout = setTimeout(() => {
+			console.log('Search for:', searchText)
+		}, delay)
+		return () => clearTimeout(searchTimeout)
+	}, [searchText])
+
 	return (
 		<nav className='flex justify-between items-center p-3 text-lg bg-purple-100'>
 			{/* Nav Logo */}
@@ -11,6 +29,19 @@ export default function Navbar() {
 			</div>
 			{/* Nav Items */}
 			<div className='flex gap-10 font-medium'>
+				{/* Search Bar */}
+				{location.pathname === '/courses' && (
+					<div className='relative w-80'>
+						<FiSearch className='absolute left-3 top-1/2 transform -translate-y-1/2 text-purple-500' />
+						<input
+							type='search'
+							value={searchText}
+							onChange={handleSearch}
+							placeholder='Search course, instructor'
+							className='w-full pl-10 pr-4 py-2 text-sm font-normal border border-gray-300 rounded-lg shadow-sm outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent'
+						/>
+					</div>
+				)}
 				<NavLink
 					to='courses'
 					className={({isActive}) =>
